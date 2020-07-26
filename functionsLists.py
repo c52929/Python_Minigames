961deba8a7663d93550ece2bb08118a11fd972b6
#c52929 <tableknocker0@gmail.com>
2020-07-26 14:19:58 +0900

    Fixed try_again function

--- functionsLists.py
+++ functionsLists.py
@@ -10,16 +10,17 @@ def exit_judge(answer):
 
 def ask_try_again(message,exitTorF):
 	try_again=input(message+' Try again? Y/N: ')
+	# print(try_again)
+	# print(try_again.lower() in answer_yes)
 	exit_judge(try_again)
 	if try_again.lower() in answer_yes:
 		print()
-		return(True)
+		return True
 	elif try_again.lower() in answer_no:
 		if exitTorF:
 			print('See you...\n')
 			exit()
 		else:
-			return
+			return False
 	else:
-		ask_try_again('Answer Y or N.',exitTorF)
-		return
+		return ask_try_again('Answer Y or N.',exitTorF)
